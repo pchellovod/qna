8b7220fdf6376247b475d203fe7fd6b503425733
pchellovod <pchellovod@gmail.com>
2017-02-28 23:33:20 +0500

    Add answer model and tests

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,8 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  it { should validate_presence_of :title }
-  it { should validate_presence_of :body }
+  describe 'association' do
+    it { should have_many(:answers).dependent(:destroy) }
+  end
 
-
+   describe 'validation' do
+     it { should validate_presence_of :title }
+     it { should validate_presence_of :body }
+   end
 end
