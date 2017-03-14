40f67af19e63e6653441a1ceb6218e5a2707276e
pchellovod <pchellovod@gmail.com>
2017-03-15 02:37:58 +0500

    add user association test to model specs

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
   describe 'association' do
-    it { should belong_to (:question)}
+    it { should belong_to (:question) }
+    it { should belong_to (:user) }
   end
 
   describe 'validation' do
