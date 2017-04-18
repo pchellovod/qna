07b6608e100eae80fbd47be40c755ceb292307d3
pchellovod <pchellovod@gmail.com>
2017-04-18 14:23:11 +0500

    add best flag to Answer

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -2,12 +2,38 @@ require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
   describe 'association' do
-    it { should belong_to (:question) }
-    it { should belong_to (:user) }
+    it { should belong_to(:question) }
+    it { should belong_to(:user) }
   end
 
   describe 'validation' do
-    it { should validate_presence_of :body}
+    it { should validate_presence_of :body }
     it { should validate_length_of(:body).is_at_least(10) }
+  end
+
+  describe 'best answer' do
+    let(:question) { create(:question) }
+    let(:answer) { create(:answer, question: question) }
+
+    it '#mark_best' do
+      answer.mark_best
+      expect(answer.reload).to be_best
+    end
+
+    it 'mark best another answer for question' do
+      create(:answer, best: true, question: question)
+      best_answer = create(:answer, question: question)
+      best_answer.mark_best
+      expect(best_answer.reload).to be_best
+    end
+
+    it 'best answer only one for question' do
+      answers = create_list(:answer, 5, question: question)
+      answers.each do |answer|
+        answer.mark_best
+        expect(answer.reload).to be_best
+        expect(question.answers.where(best: true).count).to eq 1
+      end
+    end
   end
 end
