6a7c3d44cce4e70030c49cd41093a9b1263fcd4a
pchellovod <pchellovod@gmail.com>
2017-05-10 02:11:18 +0500

    user can add several files to answer

--- spec/acceptance/answers/add_files_to_answer_spec.rb
+++ spec/acceptance/answers/add_files_to_answer_spec.rb
@@ -23,4 +23,18 @@ feature 'Add files to answer', %q{
       expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
     end
   end
+
+  scenario 'User adds several files', js: true do
+    fill_in 'Body', with: 'My answer text'
+    click_on 'Add file'
+    inputs = all('input[type="file"]')
+    inputs[0].set("#{Rails.root}/spec/spec_helper.rb")
+    inputs[1].set("#{Rails.root}/public/favicon.ico")
+    click_on 'Add new answer'
+
+    within '.answers' do
+      expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
+      expect(page).to have_link 'favicon.ico', href: '/uploads/attachment/file/2/favicon.ico'
+    end
+  end
 end
