532f5c407d40984a04402042feb1571c0900e493
dvoryankin <dvoryankindm@gmail.com>
2019-09-19 14:47:43 +0500

    add null: false to question and answer migrations

--- db/migrate/20170228181833_create_answers.rb
+++ db/migrate/20170228181833_create_answers.rb
@@ -1,8 +1,8 @@
 class CreateAnswers < ActiveRecord::Migration[5.0]
   def change
     create_table :answers do |t|
-      t.text :body
-      t.references :question, foreign_key: true
+      t.text :body, null: false
+      t.references :question, foreign_key: true, index: true
 
       t.timestamps
     end
