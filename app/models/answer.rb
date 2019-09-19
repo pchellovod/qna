d33925e5b1757ee19d1284b16e6193db34657fa2
dvoryankin <dvoryankindm@gmail.com>
2019-09-19 14:43:28 +0500

    add question at_most title validation, change the order

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,6 +1,7 @@
 class Answer < ApplicationRecord
   belongs_to :question
   belongs_to :user
+
   has_many :attachments, as: :attachable, dependent: :destroy
 
   validates :body, presence: true, length: { minimum: 10 }
